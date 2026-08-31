7f20ccb474b2dc977a42b2127a9e0472b5f9fd77
Andrew Miller <A_Miller729@yahoo.com>
2026-08-31 15:15:59 -0400

    Add House command
    Added the initial House Rhino command and verified that it executes successfully in Rhino 8.

---
+++ TechnicalAssessmentRhino/HouseCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace TechnicalAssessmentRhino
+{
+    public class HouseCommand : Command
+    {
+        public HouseCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static HouseCommand Instance { get; private set; }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName => "House";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            RhinoApp.WriteLine("House command is working!");
+
+            return Result.Success;
+        }
+    }
+}
